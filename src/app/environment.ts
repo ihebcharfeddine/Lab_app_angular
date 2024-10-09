565ef1aea18afa6e8189e2495d303ad3aea39317
ihebcharfeddine <charfeddine.iheb@enis.tn>
2024-10-09 08:42:26 +0100

    version1

---
+++ src/app/environment.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCl5_CnvbjMygOinrjnBVVIitSWLUMyd2w',
+  authDomain: 'larousse-84937.firebaseapp.com',
+  projectId: 'larousse-84937',
+  storageBucket: 'larousse-84937.appspot.com',
+  messagingSenderId: '277651758668',
+  appId: '1:277651758668:web:2582d1b83d1fca8b3e9ee9',
+  measurementId: 'G-N6CDWSR2VZ',
+};
---
+++ src/services/member.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { ReturnStatement } from '@angular/compiler';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Member } from 'src/modeles/Member';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemberService {
+  constructor(private http: HttpClient) {}
+  getAllMembers(): Observable<Member[]> {
+    //envoyer une req http vers backend
+    return this.http.get<Member[]>('http://localhost:3000/members');
+  }
+  getMemberByID(id: string): Observable<Member> {
+    return this.http.get<Member>(`http://localhost:3000/members/${id}`);
+  }
+  add(m: Member): Observable<void> {
+    return this.http.post<void>('http://localhost:3000/members', m);
+  }
+  deleteMember(id: string): Observable<void> {
+    return this.http.delete<void>(`http://localhost:3000/members/${id}`);
+  }
+  updateMember(id: string, m: Member): Observable<void> {
+    return this.http.put<void>(`http://localhost:3000/members/${id}`, m);
+  }
+}
